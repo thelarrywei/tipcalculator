21346a4eaf478bf585c627b8499e83d04e40eb95
thelarrywei <thelarrywei@gmail.com>
2014-10-10 11:17:19 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Larry Wei on 10/10/14.
+//  Copyright (c) 2014 Larry Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
